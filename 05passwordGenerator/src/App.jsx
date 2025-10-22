120407634e580d56ec878417547ce0aae29bb90f
Sush3871 <sushanrshirke96.com>
2025-10-22 17:34:12 +0530

    Remove unnecessary blank line at the beginning of App component

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
 
-
-
 function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
+  const [click, setClick] = useState("copy")
 
   //useRef hook
   const passwordRef = useRef(null)
@@ -17,21 +16,29 @@ function App() {
     if (numberAllowed) str += "0123456789"
     if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1)
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length)
       pass += str.charAt(char)
-      
+      // console.log(pass)
     }
 
     setPassword(pass)
 
 
-  }, [length, numberAllowed, charAllowed, setPassword])
+  }, [length, numberAllowed, charAllowed])
 
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
     passwordRef.current?.setSelectionRange(0, 999);
-    window.navigator.clipboard.writeText(password)
+    window.navigator.clipboard.writeText(password).then(() => {
+      setClick("Copied!");
+      setTimeout(() => {
+        setClick("copy");
+        passwordRef.current?.blur();
+      }, 5000);
+    }).catch(err => {
+      console.error('Failed to copy: ', err);
+    });
   }, [password])
 
   useEffect(() => {
@@ -53,7 +60,7 @@ function App() {
         <button
         onClick={copyPasswordToClipboard}
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-        >copy</button>
+        >{click}</button>
         
     </div>
     <div className='flex text-sm gap-x-2'>
@@ -64,14 +71,14 @@ function App() {
         max={100}
         value={length}
          className='cursor-pointer'
-         onChange={(e) => {setLength(e.target.value)}}
+         onChange={(e) => {setLength(Number(e.target.value))}}
           />
           <label>Length: {length}</label>
       </div>
       <div className="flex items-center gap-x-1">
       <input
           type="checkbox"
-          defaultChecked={numberAllowed}
+          checked={numberAllowed}
           id="numberInput"
           onChange={() => {
               setNumberAllowed((prev) => !prev);
@@ -82,7 +89,7 @@ function App() {
       <div className="flex items-center gap-x-1">
           <input
               type="checkbox"
-              defaultChecked={charAllowed}
+              checked={charAllowed}
               id="characterInput"
               onChange={() => {
                   setCharAllowed((prev) => !prev )
